08ee2a154524450a484c89e50166eba48da28ce0
Parthiv panchal <panchal.parthiv.1903@gmail.com>
2026-05-10 01:28:37 +0530

    CRU done

--- mern/restapi/index.js
+++ mern/restapi/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path"); // Access folder via path from any where
 const {v4 :uuidv4} =require('uuid');
+const methodOverride=require("method-override")
 
 const port = 8080;
 
@@ -11,6 +12,7 @@ app.use(express.static(path.join(__dirname, "public")));// folder which is acces
 
 app.use(express.urlencoded({ extended: true })); // to read respons url 
 app.use(express.json()); // parse url data (Make readable response)
+app.use(methodOverride("_method")); //Post -> PATCH
 
 
 let posts = [{
@@ -35,13 +37,14 @@ app.get("/posts/new", (req, res) => {
     res.render("new.ejs");
 });
 
+//Create new post
 app.post("/posts", (req, res) => {
     let {username, content} = req.body;
     let id = uuidv4();
     posts.push({id, username, content});
     res.redirect("/posts")
 });
-
+//Show single post
 app.get("/posts/:id",(req, res) =>{
     let{id} = req.params;
     console.log(id);
@@ -51,17 +54,24 @@ app.get("/posts/:id",(req, res) =>{
     res.render("show.ejs", { post });
 });
 
-// update quary 
-
+// update quary , Update post
 app.patch("/posts/:id",(req,res)=>{
     let {id} = req.params;
     let newContent = req.body.content;
     // console.log(newContent);
     let post = posts.find((p)=> id === p.id);
     post.content = newContent;
-    console.log(post);
-    res.send("Patch is working")
-})
+    console.log(post); 
+    // res.send("Patch is working")
+    res.redirect("/posts") ;
+});
+
+app.get("/posts/:id/edit",(req, res)=>{
+   let {id} =req.params;
+   let post = posts.find((p) => id === p.id);
+   res.render("edit.ejs",{post});
+   
+});
 
 app.listen(port, () => {
     console.log("Listening on port 8080");
